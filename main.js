404f58cc4ec00355b45225b8ef4569725593689d
lorelairaras <lorelairaras@gmail.com>
2025-07-02 17:30:37 +0800

    Nearly Finalized Version

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const shareButton = document.getElementById("share-button");
+  const popup = document.querySelector(".popup");
+  const popupText = document.getElementById("myPopup");
+
+  shareButton.addEventListener("click", function (e) {
+    e.stopPropagation();
+    popupText.classList.toggle("show");
+    popup.classList.toggle("active");
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!popup.contains(e.target)) {
+      popupText.classList.remove("show");
+      popup.classList.remove("active");
+    }
+  });
+});
